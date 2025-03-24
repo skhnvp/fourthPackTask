0f265bc87c54fa03594ea3b225daea3fe8375fc3
Pavel <59573129+skhnvp@users.noreply.github.com>
2025-03-24 21:33:10 +0300

    Update Main.java

--- src/main/java/ru/сourses/main/Main.java
+++ src/main/java/ru/сourses/main/Main.java
@@ -4,7 +4,11 @@ package ru.сourses.main;
 import java.awt.*; //import java.awt.Point;
 import ru.сourses.geometry.Point;
 
-
+/*
+Создайте и выведите на экран экземпляры двух классов: 
+Точки координат из пакета ru.сourses.geometry и точки координат из пакета java.awt. 
+Напишите код так, чтобы как минимум один из классов был доступен по простому имени.
+*/
 public class Main {
     public static void main(String[] args) {
         Point p1 = new Point();
